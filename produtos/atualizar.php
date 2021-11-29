0d66c9fad5aaedaa6ed432fdec6f0e5d5b4da989
Tiago Bezerra dos Santos <bsantostiago@gmail.com>
2021-11-29 09:00:04 -0300

    Update atualizar.php

--- produtos/atualizar.php
+++ produtos/atualizar.php
@@ -27,6 +27,7 @@ if(isset($_POST['atualizar'])){
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Produtos | UPDATE - CRUD com PHP e MySQL </title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
 <link href="../css/style.css" rel="stylesheet">
 </head>
 <body>
@@ -41,26 +42,24 @@ if(isset($_POST['atualizar'])){
     <h2>Utilize o formulário abaixo para atualizar os dados de um produto.</h2>
     
     <form action="#" method="post">
-        <!-- EXERCÍCIO: FAÇA OS DADOS DO PRODUTO
-        APARECEM EM CADA CAMPO DO FORMULÁRIO -->
-        <p>
-            <label for="nome">Nome:</label>
-	        <input value="<?=$dados['nome']?>" type="text" name="nome" id="nome" required>
+        <p class="form-group">
+            <label class="form-label" for="nome">Nome:</label>
+	        <input class="form-control" value="<?=$dados['nome']?>" type="text" name="nome" id="nome" required>
         </p>
 
-        <p>
-            <label for="preco">Preço:</label>
-	        <input value="<?=$dados['preco']?>" type="number" name="preco" id="preco" min="0" max="15000" step="0.01" required>
+        <p class="form-group">
+            <label class="form-label" for="preco">Preço:</label>
+	        <input class="form-control" value="<?=$dados['preco']?>" type="number" name="preco" id="preco" min="0" max="15000" step="0.01" required>
         </p>
 
-        <p>
-            <label for="quantidade">Quantidade:</label>
-	        <input value="<?=$dados['quantidade']?>" type="number" name="quantidade" id="quantidade" min="0" max="100" step="1" required>
+        <p class="form-group">
+            <label class="form-label" for="quantidade">Quantidade:</label>
+	        <input class="form-control" value="<?=$dados['quantidade']?>" type="number" name="quantidade" id="quantidade" min="0" max="100" step="1" required>
         </p>
 
-        <p>
-            <label for="fabricante">Fabricante:</label>
-            <select name="fabricante" id="fabricante" required>
+        <p class="form-group">
+            <label class="form-label" for="fabricante">Fabricante:</label>
+            <select class="custom-select" name="fabricante" id="fabricante" required>
                 <option value=""></option>
 
             <?php foreach($listaDeFabricantes as $dadosFabricante) { ?>
@@ -76,17 +75,20 @@ if(isset($_POST['atualizar'])){
             </select>
         </p>
     
-	    <p>
-            <label for="descricao">Descrição:</label>
-	        <textarea name="descricao" id="descricao" rows="3" cols="40" maxlength="500" required><?=$dados['descricao']?></textarea>
+	    <p class="form-group">
+            <label class="form-label" for="descricao">Descrição:</label>
+	        <textarea class="form-control" name="descricao" id="descricao" rows="3" cols="40" maxlength="500" required><?=$dados['descricao']?></textarea>
         </p>
 	    
-        <button name="atualizar">Atualizar produto</button>
+        <button class="btn btn-primary" name="atualizar">Atualizar produto</button>
 	</form>	   
 
 
 
 </div>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
